0d1f7d283160f04c858dbb9daee2293618caabd3
Thanuj <thanujdevil@gmail.com>
2024-08-09 23:55:14 +0530

    changes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import UserTable from "./UserTable";
-import Loading from "../loading"
+import Loading from "../loading";
 import Link from "next/link";
 
 interface Props {
@@ -15,8 +15,8 @@ async function UserPage({ searchParams: { sort } }: Props) {
       <Link href="/users/new" className="btn">
         New User
       </Link>
-      <Suspense fallback={<Loading/>}>
-         <UserTable sortOrder={sort} />
+      <Suspense fallback={<Loading />}>
+        <UserTable sortOrder={sort} />
       </Suspense>
     </div>
   );
