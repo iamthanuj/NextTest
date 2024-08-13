c077c0345085dab6addc4e5044bf9f484a5d09cf
Thanuj <thanujdevil@gmail.com>
2024-08-13 23:41:26 +0530

    Creation API endpoint

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request:NextRequest){
-    return NextResponse.json({"GG":"Hellow kohomdaaaa! koheda inne???"})
+    return NextResponse.json([
+        {
+            "id":1,
+            "name":"Thanuja Fernando"
+        },
+        {
+            "id":2,
+            "name":"Bishen Sathsara"
+        }
+    ])
 }
