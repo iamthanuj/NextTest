194b17028a4913909500ea9988aabcbed2c43471
Thanuj <thanujdevil@gmail.com>
2024-08-17 17:06:15 +0530

    add products

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,27 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
 
 
-export function GET(request:NextRequest){
-    return NextResponse.json([
-        {id:1, "name":"Milk", "price":2.5},
-        {id:2, "name":"Bread", "price":3.5},
-        {id:2, "name":"John", "price":4.5},
-    ])
+export async function GET(request:NextRequest){
+    const products = await prisma.products.findMany();
+    return NextResponse.json(products)
 }
 
 export async function POST(request:NextRequest){
     const body = await request.json();
     const validation = schema.safeParse(body);
 
+    const products = await prisma.products.findUnique({
+        where:{
+            name:body.name
+        }
+    })
+
     if(!validation.success){
         return NextResponse.json(validation.error.errors, {status:400});
     }
 
-    return NextResponse.json({
-        id:1,
-        name:body.name,
-        price:body.price,
+    if(products){
+        return NextResponse.json({error:"Product already exist"}, {status:400})
+    }
+
+    const newProduct = await prisma.products.create({
+        data:{
+            name:body.name,
+            price:body.price
+        }
     })
+
+    return NextResponse.json(newProduct)
 }
