bdb2b5d5ad187b928b7d008b54b67a0dfde79e80
Thanuj <thanujdevil@gmail.com>
2024-09-05 23:47:06 +0530

    server session access

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-3 p-24">
-      <h1>Hello World</h1>
+      <h1>{session && session.user!.name}</h1>
       <Link href="/users">Users</Link>
     </main>
   );
