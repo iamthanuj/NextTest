ad698404433171ab2aa03abe0051b73718a9e691
Thanuj <thanujdevil@gmail.com>
2024-09-17 03:10:38 +0530

    implement sending email

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button, Html, Container, Text, Link, Preview, Body } from "@react-email/components";
+
+const WelcomeTemplate = ({name}:{name:string}) => {
+  return (
+    <html>
+        <Preview>Welcome to N3xtTest</Preview>
+        <Body>
+            <Container>
+                <Text>Hello {name}</Text>
+                <Link href='https://github.com/iamthanuj'>Follow RetroHammer</Link>
+            </Container>
+        </Body>
+    </html>
+  )
+}
+
+export default WelcomeTemplate
