28a5b068f9e07961b44d53a172561d9ec945d33f
Thanuj <thanujdevil@gmail.com>
2024-09-18 02:33:16 +0530

    Next Image optimize

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import marco from "@/public/images/marcelo-cidrack-z4hSnLy3dQE-unsplash.jpg";
 
 export default async function Home() {
 
@@ -11,6 +12,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center gap-3 p-24">
       <h1>{session && session.user!.name}</h1>
       <Link href="/users">Users</Link>
+      <Image src={marco} alt="america" />
     </main>
   );
 }
